7d96d62ccae58daff1822461f5c093ba301be237
nirz306 <122902068+nirz306@users.noreply.github.com>
2023-12-09 17:08:31 +0530

    Create 2_studentdatabase.cpp

---
+++ 2_studentdatabase.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <string.h>     //constructor(default,parametrized),static member func,friend class,this pointer ,inline code,dynamic memory operation,exception handling 
+#include <iomanip>
+using namespace std;
+
+class Student
+{
+	public:
+    string name ,cls, bldgrp, add, drvlic,dob;
+	int rno;
+	long tele,tempS;
+
+    
+    	static int count; // Static member variable
+    	static void incrementCount()
+		 {
+        	count++;
+        	cout<<"Student "<<count<<" data succesfully added."<<endl;
+    	}
+    
+        Student() //Default Constructor
+        {
+            name=""; cls="";bldgrp="";add="";drvlic="";dob="";
+            rno=0; tele=0;
+        }
+
+        Student(string name, string cls, string bldgrp, string add, string drvlic, string dob, int rno, int tele, int aadhaar) //paremeterised constructor
+        {
+        	this->name = name;
+        	this->cls = cls;
+        	this->bldgrp = bldgrp;
+        	this->add = add;
+        	this->drvlic = drvlic;
+        	this->dob = dob;
+        	this->rno = rno;
+        	this->tele = rno;
+        }
+
+	    Student(const Student &s1) //Copy Constructor
+	    {
+	        name = s1.name;
+	        cls = s1.cls;
+	        bldgrp = s1.bldgrp;
+	        add = s1.add;
+	        drvlic = s1.drvlic;
+	        dob = s1.dob;
+	        rno = s1.rno;
+	        tele = s1.tele;
+	    }
+
+	
+	    void input()
+	    {
+	        cout<<"Enter Name of Student: ";
+			cin>>name;
+			cout<<"\nEnter Class: ";
+			cin>>cls;
+			cout<<"\nEnter Blood Group: ";
+			cin>>bldgrp;
+			cout<<"\nEnter Address: ";
+			cin>>add;
+			cout<<"\nEnter Driving licence no: ";
+			cin>>drvlic;
+			cout<<"\nEnter Date of birth: ";
+			cin>>dob;
+			cout<<"\nEnter roll no: ";
+			cin>>rno;
+					//contact number with exception handling
+		while(true)
+	{
+		cout << "Enter your contact number = ";
+		try
+		{
+			cin >> tempS;
+			string numString = to_string(tempS);
+    		int length = numString.length();
+			if (length!=10){
+				throw "Phone number must be 10 digit!";
+			}
+		 	tele = tempS;
+			break;
+		}
+		catch (const char *msg){
+			cout << msg << endl;
+			cin.clear();
+			cin.ignore(1000,'\n');
+		}
+	}
+			cout<<"\n\n\n\n";
+	    }
+
+	
+		inline void display()
+		{
+			cout<<left<<setw(30)<<name<<setw(30)<<cls<<setw(30)<<bldgrp<<setw(30)<<add<<setw(30)<<drvlic<<setw(30)<<dob<<setw(30)<<rno<<setw(30)<<tele<<endl;
+		}
+
+		~ Student() //Destructor
+		{
+			cout<<"Destructor Invoked"<<endl;
+		}
+
+    friend class Hello;
+};
+
+class Hello
+{
+    public:
+        void display(Student &s)
+        {	
+			cout<<"Hello"<<s.name<<endl;
+       }
+};
+
+int Student::count=0;  //by syntax
+int main()
+{
+	Student s1,s2,s3,s4;
+	Hello h1,h2,h3,h4;
+	s1.input();
+	Student::incrementCount();
+	s2.input();
+	Student::incrementCount();
+	s3.input();
+	Student::incrementCount();
+	s4.input();
+	Student::incrementCount();
+	cout<<left<<setw(30)<<"NAME"<<setw(30)<<"CLASS"<<setw(30)<<"BLOOD GROUP"<<setw(30)<<"ADDRESS"<<setw(30)<<"DRIVING LIC NO"<<setw(30)<<"DATE OF BIRTH"<<setw(30)<<"ROLL NO"<<setw(30)<<"TELEPHONE"<<"\n============================================================================================="<<endl;
+	h1.display(s1);
+	h2.display(s2);
+	h3.display(s3);
+	h4.display(s4);
+	Hello *h = new Hello[3];
+	delete [] h;
+}
